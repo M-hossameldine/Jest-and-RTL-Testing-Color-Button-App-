ba7b8eef9fa7b426d6a48e3656dac2151be01c4b
Mohamed Hossam <m.hossameldine@gmail.com>
2022-08-14 10:10:32 +0200

    Test and code button click

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  const [btnClr, setBtnClr] = useState('red');
+
+  const toggleClrHandler = () => {
+    setBtnClr((clr) => (clr === 'red' ? 'blue' : 'red'));
+  };
+
+  const btnStyle = { backgroundColor: btnClr === 'red' ? 'red' : 'blue' };
+  const btnText = `Change to ${btnClr === 'red' ? 'blue' : 'red'}`;
+
+  return (
+    <div className='App'>
+      <button type='button' style={btnStyle} onClick={toggleClrHandler}>
+        {btnText}
+      </button>
+    </div>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,2 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+
+test('the button initial color is red', () => {
+  render(<App />);
+
+  // get element by role button that contains text 'Change color to blue'
+  const colorButton = screen.getByRole('button', 'Change to blue');
+
+  // expect button initial color to be 'red'
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+
+  // click on the button
+  fireEvent.click(colorButton);
+
+  // expect button initial color to be 'blue'
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+
+  // expect button text to be 'Change color to red'
+  expect(colorButton.textContent).toEqual('Change to red');
+});
